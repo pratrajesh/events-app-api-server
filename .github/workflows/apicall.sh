037a2dde86a54bd036e38570230deb4a03aef7c8
pratrajesh <66231784+pratrajesh@users.noreply.github.com>
2023-02-10 12:15:37 +0530

    Update apicall.sh

--- .github/workflows/apicall.sh
+++ .github/workflows/apicall.sh
@@ -1,1 +1,10 @@
 
+str="4w 2d 1h"
+regex="([0-9])w ([0-9])d ([0-9])h"
+if [[ $str =~ $regex ]]
+then
+    week="${BASH_REMATCH[1]}"
+    day="${BASH_REMATCH[2]}"
+    hour="${BASH_REMATCH[3]}"
+    echo $week --- $day ---- $hour
+fi
